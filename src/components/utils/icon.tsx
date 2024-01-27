3101a911b8539284cdafe4e3177397f596a103ac
Kacper <andziakwozniak@gmail.com>
2024-01-27 22:26:43 +0100

    rm fortawesome

--- src/components/utils/icon.tsx
+++ src/components/utils/icon.tsx
@@ -4,7 +4,7 @@ import React, {useEffect, useState} from 'react';
 // import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 // import {fas} from '@fortawesome/pro-solid-svg-icons';
 // import {fat} from '@fortawesome/pro-thin-svg-icons';
-// import {fal} from '@fortawesome/pro-light-svg-icons';
+// import {fal} from '@fortawesome/pro-light-svg-icons'; 
 // import {fad} from '@fortawesome/pro-duotone-svg-icons';
 // import {far} from '@fortawesome/pro-regular-svg-icons';
 // import {fab} from '@fortawesome/free-brands-svg-icons';
